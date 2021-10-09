c4e11731572cb0d5b3c80381e090ed6645a5e803
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-10-09 22:16:53 +0300

    react-native-svg/transformers

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import { SafeAreaView,TouchableOpacity, Text, View } from 'react-native'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import GamingImg from './assets/game.svg'
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text style={{fontFamily: 'Montserrat-Light', fontSize: 30}} >this is just for truing</Text>
-      <MaterialIcons name='search' size={22} />
+    <SafeAreaView
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            fontFamily: 'Montserrat-Light',
+            fontSize: 30,
+            color: '#20315F'
+            }}>
+            GAMEON
+        </Text>
+      </View>
+    <GamingImg width={300} height={300} />
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%',
+        backgroundColor: '#ad40af',
+        padding: 20,
+        borderRadius: 5,
+        }} >
+      <Text style={{color: 'white', fontSize: 18, fontFamily: 'Roboto-MediumItalic'}} >
+        Let's Begin
+      </Text>
+      <View>
+          <MaterialIcons name='arrow-forward-ios' size={32} color='white'/>
+        </View>
+    </TouchableOpacity>
+      
     </SafeAreaView>
   )
 }
