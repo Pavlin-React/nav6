9b6a899dabb6caef7496acb534749e863e87290d
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-10-10 10:53:57 +0300

    react-navigation-6

--- App.js
+++ App.js
@@ -2,8 +2,23 @@ import React from 'react'
 import { SafeAreaView,TouchableOpacity, Text, View } from 'react-native'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import GamingImg from './assets/game.svg'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+let Stack = createNativeStackNavigator()
+
+let App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Main' component={Main} options={{headerShown: false}} />
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const Main = ({navigation}) => {
   return (
     <SafeAreaView
       style={{
@@ -13,7 +28,7 @@ const App = () => {
         alignItems: 'center'
       }}
     >
-      <View>
+      <View style={{marginTop: 20}} >
         <Text
           style={{
             fontFamily: 'Montserrat-Light',
@@ -23,8 +38,11 @@ const App = () => {
             GAMEON
         </Text>
       </View>
-    <GamingImg width={300} height={300} />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+      <GamingImg width={300} height={300} style={{transform: [{rotate: '-15deg'}]}} />
+    </View>
     <TouchableOpacity
+      onPress={() => navigation.navigate('Home')}
       style={{
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -33,6 +51,8 @@ const App = () => {
         backgroundColor: '#ad40af',
         padding: 20,
         borderRadius: 5,
+        marginBottom: 50,
+
         }} >
       <Text style={{color: 'white', fontSize: 18, fontFamily: 'Roboto-MediumItalic'}} >
         Let's Begin
@@ -43,6 +63,16 @@ const App = () => {
     </TouchableOpacity>
       
     </SafeAreaView>
+  )
+}
+
+let Home = () => {
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+    <Text>
+      Home Screen
+    </Text>
+  </View>
   )
 }
 
